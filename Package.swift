478384772fcd96f2a06b31885c4e3f57a961a8ef
Zach Eriksen <zmeriksen@icloud.com>
2021-11-30 19:21:56 -0700

    Update file structure

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "StaticSite",
+    products: [
+        .executable(
+            name: "StaticSite",
+            targets: ["StaticSite"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0")
+    ],
+    targets: [
+        .target(
+            name: "StaticSite",
+            dependencies: [
+                "Publish",
+                "SplashPublishPlugin"
+            ]
+        )
+    ]
+)
