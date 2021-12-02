edfde37bb4272a62286e476382d0e4df5d04758f
Zach Eriksen <zmeriksen@icloud.com>
2021-12-01 17:43:00 -0700

    Add CNAMEPublishPlugin

--- Package.swift
+++ Package.swift
@@ -12,13 +12,15 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
-        .package(url: "https://github.com/alex-ross/highlightjspublishplugin", from: "1.0.0")
+        .package(url: "https://github.com/alex-ross/highlightjspublishplugin", from: "1.0.0"),
+        .package(url: "https://github.com/SwiftyGuerrero/CNAMEPublishPlugin", from: "0.1.0")
     ],
     targets: [
         .target(
             name: "StaticSite",
             dependencies: [
                 "Publish",
+                "CNAMEPublishPlugin",
                 .product(name: "HighlightJSPublishPlugin", package: "highlightjspublishplugin")
             ]
         )
